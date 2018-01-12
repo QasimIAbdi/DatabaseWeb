6cf5de0b147fb9b4d288e3d32acd83728aa06309
Tom <viettran_95@hotmail.com>
2018-01-12 12:15:11 +0100

    Merge branch 'master' into origin/master

---
+++ test1Tom.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Databaser og web</title>
+<meta charset="UTF-8" />
+</head>
+<body>
+
+<h1> PHP-demo</h1>
+
+<?php 
+for ($i=0; $i<=100; $i++) {
+print("<p> Dette er avsnitt $i.</p>\n");
+}
+?>
+
+</body>
+</html>
